4038c9d99ff3ab2f41f33305de75bea6c64dcbef
AmulyaThammineni <147725657+AmulyaThammineni@users.noreply.github.com>
2024-11-07 22:15:26 +0530

    Add files via upload

--- carRentalSystem/src/main/java/com/infosys/carRentalSystem/dao/CustomerRepository.java
+++ carRentalSystem/src/main/java/com/infosys/carRentalSystem/dao/CustomerRepository.java
@@ -2,6 +2,8 @@ package com.infosys.carRentalSystem.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.infosys.carRentalSystem.bean.Customer;
+
 public interface CustomerRepository extends JpaRepository<Customer ,String>{
 	
 }
